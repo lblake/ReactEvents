120e255f9990dc5a9ff345b0b121653cea3856c6
Lloyd <lloyd@lloyds-mbp.home>
2019-10-31 16:28:50 +0000

    added components for left and right columns

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
 
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div>
         <h1>Re-vents</h1>
+        <EventDashboard/>
       </div>
     );
   }
